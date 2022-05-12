a523f8abf0ba80725faef512de61e3863fb4b5b6
汪光武 <wangguangwu@pukanghealth.com>
2022-05-12 10:34:03 +0800

    init

---
+++ src/main/java/com/wangguangwu/Test.java
@@ -0,0 +1,15 @@
+package com.wangguangwu;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author wangguangwu
+ */
+@Slf4j
+public class Test {
+
+    public static void main(String[] args) {
+        log.info("Hello World");
+    }
+
+}
